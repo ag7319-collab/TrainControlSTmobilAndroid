b78acd13e61fcc0ffc1077e540164c02cab4c33a
ag7319-collab <ag7319@gmail.com>
2026-09-20 14:01:03 +0200

    Version 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.traincontrolstmobilandroid"
         minSdk = 31
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.1.1"
+        versionCode = 2
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
